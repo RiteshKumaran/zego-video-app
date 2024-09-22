8bd151864cf6744f1735bf97a9acd599819e3e22
RiteshKumaran <riteshkumaran.dev@gmail.com>
2024-09-22 15:03:54 +0530

    update vercel.json

--- videoconferencing/settings.py
+++ videoconferencing/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-6fdgh7tki&^y_-cl4%tpyl-$kh9($vos&a5q&uq%!az2gs__bz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'zego-video-app.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 
 # Application definition
