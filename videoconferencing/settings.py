903f6466bf4ca5dee6b117843890d4ea70308958
RiteshKumaran <riteshkumaran.dev@gmail.com>
2024-09-22 14:31:45 +0530

    fix

--- videoconferencing/settings.py
+++ videoconferencing/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-6fdgh7tki&^y_-cl4%tpyl-$kh9($vos&a5q&uq%!az2gs__bz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['django-video-conferencing.vercel.app']
 
 
 # Application definition
